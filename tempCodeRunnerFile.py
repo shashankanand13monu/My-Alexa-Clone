bfa849d677a3de16fdf084cf40ff4ab785e31594
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2021-10-21 19:59:21 +0530

    Added Demo

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ome_path="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" #CHANGE ACC. TO NEED
+# webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),preferred=True)
+# wb= webbrowser.get('chrome')
+# # wb.open_new_tab("google.com")
+# open_code= "C:\\Users\\KIIT\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+# music_dir = r"C:\Users\KIIT\Desktop\Apps\Project\My Alexa Assistant\Music" #r-> Spaces in b/w or use C:\\xx\\xx
+# songs = os.listdir(music_dir)
